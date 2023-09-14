992d12e7ed6f9a04120815e93dab780c5dafec95
Roman Gurovich <roman@Romans-MacBook-Pro.local>
2023-09-14 11:41:32 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+
+import p5 from 'p5';
+import './styles.css';
+
+let angle = 0; // Initial rotation angle
+let width = 524;
+let height = 524;
+let font;
+let canvas;
+let layer;
+
+let sketch = (p) => {    
+  p.preload = () => {
+    font = p.loadFont('../fonts/JenrivTitling-Regular.otf')
+  };
+
+  p.setup = () =>{
+    canvas = p.createCanvas(width, height, P5.WEBGL, document.getElementById('myCanvas'));
+    layer = p.createFramebuffer();
+  };
+
+  p.draw = () =>{
+    p.clear();
+
+    layer.begin()
+    p.clear();
+
+    // Set rotation angle
+    angle += 0.01;
+
+    // Rotate around the Y-axis
+    p.rotateY(angle);
+    p.rotateZ(angle);
+
+    // Draw the 0" shape
+    p.fill(255, 204, 0);
+    p.textFont(font);
+    p.textSize(200);
+    p.text("0", 0, 0);
+    
+    layer.end()
+
+    // Draw the 1" shape
+    p.rotateY(angle, 0)
+    p.translate(-100, 0);
+    p.fill(192, 192, 192);
+    p.textFont(font);
+    p.textSize(200);
+    p.text("1", 50, 50);
+    p.image(layer, 0, 0,width / 2, height / 2)
+  }
+};
+
+const P5 = new p5(sketch);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        https: false,
+    },
+    // optimization: {
+    //     runtimeChunk: 'single'
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/', // Output directory for fonts
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+};
